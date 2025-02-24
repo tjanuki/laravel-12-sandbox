632556d35c4997ea24cdc1f6310d783ec4c531ee
tjanuki <t.januki@gmail.com>
2025-02-24 16:03:58 -0500

    Add starter kit

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -23,7 +23,9 @@ defineProps<{
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                    <div class="flex justify-center items-center h-full">
+                        Hello world
+                    </div>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
