d3842745ffcbdbd7f0fe48e555c1abb3464ad583
tjanuki <t.januki@gmail.com>
2025-02-25 11:20:35 -0500

    Add switch component

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Switch } from '@/components/ui/switch';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
@@ -23,9 +24,7 @@ defineProps<{
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <div class="flex justify-center items-center h-full">
-                        Hello world
-                    </div>
+                    <div class="flex h-full items-center justify-center">Hello world <Switch /></div>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
